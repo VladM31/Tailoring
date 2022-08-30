791ab0e4ace7efdfd97318888fea05eb22b260fc
Vladyslav Mormul <vladyslav.mormul@nure.ua>
2022-08-30 14:55:59 +0300

    init

---
+++ src/main/java/sigma/nure/tailoring/tailoring/TailoringApplication.java
@@ -0,0 +1,13 @@
+package sigma.nure.tailoring.tailoring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TailoringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TailoringApplication.class, args);
+    }
+
+}
