9e45b7d1afed3a9da521727c9d3e89fb95c0931b
Vladyslav Mormul <vladyslav.mormul@nure.ua>
2022-09-01 22:20:28 +0300

    created bean for user repository #8

---
+++ src/main/java/sigma/nure/tailoring/tailoring/config/RepositoryConfig.java
@@ -0,0 +1,20 @@
+package sigma.nure.tailoring.tailoring.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.JdbcTemplate;
+import sigma.nure.tailoring.tailoring.entities.Role;
+import sigma.nure.tailoring.tailoring.repository.UserRepository;
+import sigma.nure.tailoring.tailoring.repository.UserRepositoryJdbcTemplatePostgres;
+
+import javax.sql.DataSource;
+import java.util.Map;
+
+@Configuration
+public class RepositoryConfig {
+
+    @Bean
+    public UserRepository userRepositoryJdbcTemplatePostgres(JdbcTemplate jdbc,DataSource dataSource, Map<Role,Integer> idByRole){
+        return new UserRepositoryJdbcTemplatePostgres(jdbc,dataSource,idByRole);
+    }
+}
