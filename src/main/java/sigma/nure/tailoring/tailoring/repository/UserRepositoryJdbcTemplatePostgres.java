fd1d7d8c4c8454af58d45ddc8a3d8a8fbd44f1f3
Vladyslav Mormul <vladyslav.mormul@nure.ua>
2022-09-01 21:50:40 +0300

    implemented methods for find users #8

--- src/main/java/sigma/nure/tailoring/tailoring/repository/UserRepositoryJdbcTemplatePostgres.java
+++ src/main/java/sigma/nure/tailoring/tailoring/repository/UserRepositoryJdbcTemplatePostgres.java
@@ -1,6 +1,8 @@
 package sigma.nure.tailoring.tailoring.repository;
 
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.jdbc.core.namedparam.SqlParameterSource;
 import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
@@ -24,35 +26,61 @@ public class UserRepositoryJdbcTemplatePostgres implements UserRepository{
     private final Map<Role,Integer> idByRole;
     private final JdbcTemplate jdbc;
     private final SimpleJdbcInsert insertUser;
+    private final RowMapper<User> rowMapper;
 
     public UserRepositoryJdbcTemplatePostgres(JdbcTemplate jdbc, DataSource dataSource,Map<Role,Integer> idByRole) {
         this.jdbc = jdbc;
+        this.idByRole = idByRole;
+        this.rowMapper = new BeanPropertyRowMapper<>(User.class);
+
         this.insertUser = new SimpleJdbcInsert(dataSource)
                 .withTableName("\"user\"")
                 .usingGeneratedKeyColumns("id")
-                .usingColumns("phone_number","password","city","country","email","firstname","lastname","date_registration","male","user_state","role_id");
-        this.idByRole = idByRole;
+                .usingColumns("phone_number","password","city","country","email","firstname",
+                        "lastname","date_registration","male","user_state","role_id");
     }
 
+    private static final String SELECT_USER = "SELECT u.id, u.password, u.city, " +
+            "u.country, u.email, u.firstname, u.lastname, u.active, u.male, " +
+            "u.user_state AS userState, " +
+            "u.phone_number AS phoneNumber, " +
+            "u.date_registration AS dateRegistration, " +
+            "r.name AS role " +
+            "FROM \"user\" u " +
+            "LEFT JOIN role r ON r.id = u.role_id ";
 
+    private static final String SELECT_ORDER_BY_LIMIT_OFFSET = SELECT_USER + " ORDER BY %s %s LIMIT %s OFFSET %s";
     @Override
     public List<User> findAll(Pageable pageable) {
-        return null;
+        return jdbc.query(String.format(SELECT_ORDER_BY_LIMIT_OFFSET,
+                pageable.getOrderByOrDefault("date_registration"),
+                pageable.getDirectionOrDefault(Pageable.Direction.DESC),
+                pageable.getLimitOrDefault(100L),
+                pageable.getOffsetOrDefault(0L)),
+                this.rowMapper);
     }
 
+    private static final String SELECT_USER_BY_ID = SELECT_USER + " WHERE u.id = ? ";
     @Override
     public Optional<User> findById(Long id) {
-        return Optional.empty();
+        return jdbc.queryForStream(SELECT_USER_BY_ID,this.rowMapper,id).findFirst();
     }
 
+    private static final String SELECT_USER_WHERE_PHONE_NUMBER_AND_ACTIVE_TRUE_AND_STATE_REGISTERED =
+            SELECT_USER + " WHERE u.phone_number = ? AND u.active = true AND u.user_state = ? ";
     @Override
     public Optional<User> findByPhoneNumberAndActiveTrueAndUserStateRegistered(String number) {
-        return Optional.empty();
+        return jdbc.queryForStream(SELECT_USER_WHERE_PHONE_NUMBER_AND_ACTIVE_TRUE_AND_STATE_REGISTERED,
+                this.rowMapper,number,UserState.REGISTERED).findFirst();
     }
 
+    private static final String SELECT_USER_WHERE_CODE_AND_NUMBER_ID_AND_DATE_OD_CREATION_BEFORE = SELECT_USER +
+            " RIGHT JOIN user_code c ON u.id = c.user_id " +
+            " WHERE c.value = ?  AND  u.phone_number = ? AND c.user_code < ? ";
     @Override
     public Optional<User> findByUserCodeAndPhoneNumberAndActiveTrueAndDateOfCreationBefore(String code, String number, LocalDateTime dateOfCreation) {
-        return Optional.empty();
+        return jdbc.queryForStream(SELECT_USER_WHERE_CODE_AND_NUMBER_ID_AND_DATE_OD_CREATION_BEFORE,
+                this.rowMapper,code,number,dateOfCreation).findFirst();
     }
 
     @Override
