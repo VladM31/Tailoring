aab359a78cde5661b06e81f0d67a7533637635d7
Vladyslav Mormul <vladyslav.mormul@nure.ua>
2022-09-01 16:34:27 +0300

    Created interface for works with data user from database #8

---
+++ src/main/java/sigma/nure/tailoring/tailoring/repository/UserRepository.java
@@ -0,0 +1,25 @@
+package sigma.nure.tailoring.tailoring.repository;
+
+
+import org.springframework.lang.NonNull;
+import sigma.nure.tailoring.tailoring.entities.User;
+import sigma.nure.tailoring.tailoring.entities.UserState;
+import sigma.nure.tailoring.tailoring.tools.Pageable;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
+
+public interface UserRepository {
+   List<User> findAll(@NonNull Pageable pageable);
+   Optional<User> findById(@NonNull Long id);
+   Optional<User> findByPhoneNumberAndActiveTrueAndUserStateRegistered(String number);
+   Optional<User> findByUserCodeAndPhoneNumberAndActiveTrueAndDateOfCreationBefore(String code, String number, LocalDateTime dateOfCreation);
+
+   boolean save(@NonNull User user);
+   boolean isBooked(String email,String phoneNumber);
+
+   boolean update(@NonNull User user);
+   boolean updateActiveById(boolean active,Long userId);
+   boolean updateUserStateById(UserState userState,Long userId);
+}
