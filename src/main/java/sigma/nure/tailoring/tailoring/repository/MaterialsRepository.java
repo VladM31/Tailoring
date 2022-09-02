31716f9db01ef02d93a33c6da25926b6ac68939c
Vladyslav Mormul <vladyslav.mormul@nure.ua>
2022-09-02 18:27:46 +0300

    created repository for Materials #12

---
+++ src/main/java/sigma/nure/tailoring/tailoring/repository/MaterialsRepository.java
@@ -0,0 +1,15 @@
+package sigma.nure.tailoring.tailoring.repository;
+
+import sigma.nure.tailoring.tailoring.entities.Color;
+import sigma.nure.tailoring.tailoring.entities.Material;
+
+import java.util.List;
+
+public interface MaterialsRepository {
+    List<Material> findAllMaterial();
+    boolean updateMaterial(Material material);
+    boolean saveMaterial(Material material);
+    List<Color> findAllColors();
+    boolean updateColor(Color color);
+    boolean saveColor(Color color);
+}
