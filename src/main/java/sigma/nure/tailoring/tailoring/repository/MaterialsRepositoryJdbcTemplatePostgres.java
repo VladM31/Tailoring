789c5f3a884df328b36d73012d2605a94784727d
Vladyslav Mormul <vladyslav.mormul@nure.ua>
2022-09-02 19:36:14 +0300

    implemented methods for work with color #12

--- src/main/java/sigma/nure/tailoring/tailoring/repository/MaterialsRepositoryJdbcTemplatePostgres.java
+++ src/main/java/sigma/nure/tailoring/tailoring/repository/MaterialsRepositoryJdbcTemplatePostgres.java
@@ -13,18 +13,20 @@ import java.util.List;
 public class MaterialsRepositoryJdbcTemplatePostgres implements MaterialsRepository{
 
     private final JdbcTemplate jdbc;
-    private final RowMapper<Material> rowMapper;
+    private final RowMapper<Material> materialRowMapper;
+    private final RowMapper<Color> colorRowMapper;
 
 
     public MaterialsRepositoryJdbcTemplatePostgres(JdbcTemplate jdbc) {
         this.jdbc = jdbc;
-        this.rowMapper = new BeanPropertyRowMapper<>(Material.class);
+        this.materialRowMapper = new BeanPropertyRowMapper<>(Material.class);
+        this.colorRowMapper = new BeanPropertyRowMapper<>(Color.class);
     }
 
     private static final String SELECT_ALL_MATERIALS = "SELECT id, name, cost_one_square_meter AS cost FROM material";
     @Override
     public List<Material> findAllMaterial() {
-        return jdbc.query(SELECT_ALL_MATERIALS,rowMapper);
+        return jdbc.query(SELECT_ALL_MATERIALS,materialRowMapper);
     }
 
     private static final String UPDATE_MATERIAL_BY_ID = "UPDATE material SET name = ?, cost_one_square_meter = ? WHERE id = ?";
@@ -39,18 +41,21 @@ public class MaterialsRepositoryJdbcTemplatePostgres implements MaterialsReposit
         return jdbc.update(INSERT_MATERIAL,material.getName(),material.getCost()) != 0;
     }
 
+    private static final String SELECT_ALL_COLORS = "SELECT id, name, color_code AS code FROM color";
     @Override
     public List<Color> findAllColors() {
-        return null;
+        return jdbc.query(SELECT_ALL_COLORS,this.colorRowMapper);
     }
 
+    private static final String UPDATE_COLORS_BY_ID = "UPDATE color SET name = ?, color_code = ? WHERE id = ?";
     @Override
     public boolean updateColor(Color color) {
-        return false;
+        return jdbc.update(UPDATE_COLORS_BY_ID,color.getName(),color.getCode(),color.getId()) != 0;
     }
 
+    private static final String INSERT_COLOR = "INSERT INTO color(name,color_code) VALUES(?,?)";
     @Override
     public boolean saveColor(Color color) {
-        return false;
+        return jdbc.update(INSERT_COLOR,color.getName(),color.getCode()) != 0;
     }
 }
