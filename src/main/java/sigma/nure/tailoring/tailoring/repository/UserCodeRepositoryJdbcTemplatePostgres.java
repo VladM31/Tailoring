8b52deabb1648833d69766d048d41e5078874f2d
Vladyslav Mormul <vladyslav.mormul@nure.ua>
2022-09-04 13:49:10 +0300

    update code style for UserCodeRepository #11

--- src/main/java/sigma/nure/tailoring/tailoring/repository/UserCodeRepositoryJdbcTemplatePostgres.java
+++ src/main/java/sigma/nure/tailoring/tailoring/repository/UserCodeRepositoryJdbcTemplatePostgres.java
@@ -5,11 +5,11 @@ import org.springframework.jdbc.core.JdbcTemplate;
 
 import java.time.LocalDateTime;
 
-public class UserCodeRepositoryJdbcTemplatePostgres implements UserCodeRepository{
+public class UserCodeRepositoryJdbcTemplatePostgres implements UserCodeRepository {
 
     private final JdbcTemplate jdbc;
 
-    public UserCodeRepositoryJdbcTemplatePostgres(JdbcTemplate jdbc){
+    public UserCodeRepositoryJdbcTemplatePostgres(JdbcTemplate jdbc) {
         this.jdbc = jdbc;
     }
 
@@ -17,14 +17,16 @@ public class UserCodeRepositoryJdbcTemplatePostgres implements UserCodeRepositor
             "UPDATE user_code SET active = false " +
                     "WHERE user_id = ? " +
                     "AND value = ?";
+
     @Override
     public boolean updateCodeToInactiveByUserIdAndCodValue(Long userId, String codeValue) {
-        return jdbc.update(UPDATE_INACTIVE_BY_USER_ID_AND_CODE_VALUE,userId,codeValue) != 0;
+        return jdbc.update(UPDATE_INACTIVE_BY_USER_ID_AND_CODE_VALUE, userId, codeValue) != 0;
     }
 
     private static final String INSERT_USER_CODE = "INSERT INTO user_code(user_id,value,date_of_creation) VALUES(?,?,?)";
+
     @Override
     public boolean save(Long userId, String codeValue) {
-        return jdbc.update(INSERT_USER_CODE,userId,codeValue, LocalDateTime.now()) != 0;
+        return jdbc.update(INSERT_USER_CODE, userId, codeValue, LocalDateTime.now()) != 0;
     }
 }
