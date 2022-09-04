6073e7658c30f84133c828b93b1e8bf34c38787b
Vladyslav Mormul <vladyslav.mormul@nure.ua>
2022-09-04 13:49:56 +0300

    removed service #11

--- src/main/java/sigma/nure/tailoring/tailoring/service/UserCodeService.java
+++
@@ -1,7 +0,0 @@
-package sigma.nure.tailoring.tailoring.service;
-
-public interface UserCodeService {
-    boolean updateCodeToInactiveByUserIdAndCodValue(Long userId, String codeValue);
-
-    boolean save(Long userId, String codeValue);
-}
