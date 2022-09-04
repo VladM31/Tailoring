c50480e7d88e8cadcec8b42b5b8bc6690c250983
Vladyslav Mormul <vladyslav.mormul@nure.ua>
2022-09-04 17:21:04 +0300

    Fix code. Changed the position of variables #11

--- src/main/java/sigma/nure/tailoring/tailoring/repository/UserCodeRepositoryJdbcTemplatePostgres.java
+++ src/main/java/sigma/nure/tailoring/tailoring/repository/UserCodeRepositoryJdbcTemplatePostgres.java
@@ -7,23 +7,23 @@ import java.time.LocalDateTime;
 
 public class UserCodeRepositoryJdbcTemplatePostgres implements UserCodeRepository {
 
+    private static final String UPDATE_INACTIVE_BY_USER_ID_AND_CODE_VALUE =
+            "UPDATE user_code SET active = false " +
+                    "WHERE user_id = ? " +
+                    "AND value = ?";
+
+    private static final String INSERT_USER_CODE = "INSERT INTO user_code(user_id,value,date_of_creation) VALUES(?,?,?)";
+
     private final JdbcTemplate jdbc;
 
     public UserCodeRepositoryJdbcTemplatePostgres(JdbcTemplate jdbc) {
         this.jdbc = jdbc;
     }
 
-    private static final String UPDATE_INACTIVE_BY_USER_ID_AND_CODE_VALUE =
-            "UPDATE user_code SET active = false " +
-                    "WHERE user_id = ? " +
-                    "AND value = ?";
-
     @Override
     public boolean updateCodeToInactiveByUserIdAndCodValue(Long userId, String codeValue) {
         return jdbc.update(UPDATE_INACTIVE_BY_USER_ID_AND_CODE_VALUE, userId, codeValue) != 0;
     }
-
-    private static final String INSERT_USER_CODE = "INSERT INTO user_code(user_id,value,date_of_creation) VALUES(?,?,?)";
 
     @Override
     public boolean save(Long userId, String codeValue) {
