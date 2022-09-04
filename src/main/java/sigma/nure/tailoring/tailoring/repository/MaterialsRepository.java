ab773e06aa75d4540574d28d742a6302e54ce0ad
Vladyslav Mormul <vladyslav.mormul@nure.ua>
2022-09-04 13:51:23 +0300

    update code style #12

--- src/main/java/sigma/nure/tailoring/tailoring/repository/MaterialsRepository.java
+++ src/main/java/sigma/nure/tailoring/tailoring/repository/MaterialsRepository.java
@@ -7,9 +7,14 @@ import java.util.List;
 
 public interface MaterialsRepository {
     List<Material> findAllMaterial();
+
     boolean updateMaterial(Material material);
+
     boolean saveMaterial(Material material);
+
     List<Color> findAllColors();
+
     boolean updateColor(Color color);
+
     boolean saveColor(Color color);
 }
--- src/main/java/sigma/nure/tailoring/tailoring/repository/MaterialsRepositoryJdbcTemplatePostgres.java
+++ src/main/java/sigma/nure/tailoring/tailoring/repository/MaterialsRepositoryJdbcTemplatePostgres.java
@@ -9,8 +9,7 @@ import sigma.nure.tailoring.tailoring.entities.Material;
 
 import java.util.List;
 
-@Repository
-public class MaterialsRepositoryJdbcTemplatePostgres implements MaterialsRepository{
+public class MaterialsRepositoryJdbcTemplatePostgres implements MaterialsRepository {
 
     private final JdbcTemplate jdbc;
     private final RowMapper<Material> materialRowMapper;
@@ -24,38 +23,44 @@ public class MaterialsRepositoryJdbcTemplatePostgres implements MaterialsReposit
     }
 
     private static final String SELECT_ALL_MATERIALS = "SELECT id, name, cost_one_square_meter AS cost FROM material";
+
     @Override
     public List<Material> findAllMaterial() {
-        return jdbc.query(SELECT_ALL_MATERIALS,materialRowMapper);
+        return jdbc.query(SELECT_ALL_MATERIALS, materialRowMapper);
     }
 
     private static final String UPDATE_MATERIAL_BY_ID = "UPDATE material SET name = ?, cost_one_square_meter = ? WHERE id = ?";
+
     @Override
     public boolean updateMaterial(Material material) {
-        return jdbc.update(UPDATE_MATERIAL_BY_ID,material.getName(),material.getCost(),material.getId()) != 0;
+        return jdbc.update(UPDATE_MATERIAL_BY_ID, material.getName(), material.getCost(), material.getId()) != 0;
     }
 
     private static final String INSERT_MATERIAL = "INSERT INTO material(name,cost_one_square_meter) VALUES(?,?)";
+
     @Override
     public boolean saveMaterial(Material material) {
-        return jdbc.update(INSERT_MATERIAL,material.getName(),material.getCost()) != 0;
+        return jdbc.update(INSERT_MATERIAL, material.getName(), material.getCost()) != 0;
     }
 
     private static final String SELECT_ALL_COLORS = "SELECT id, name, color_code AS code FROM color";
+
     @Override
     public List<Color> findAllColors() {
-        return jdbc.query(SELECT_ALL_COLORS,this.colorRowMapper);
+        return jdbc.query(SELECT_ALL_COLORS, this.colorRowMapper);
     }
 
     private static final String UPDATE_COLORS_BY_ID = "UPDATE color SET name = ?, color_code = ? WHERE id = ?";
+
     @Override
     public boolean updateColor(Color color) {
-        return jdbc.update(UPDATE_COLORS_BY_ID,color.getName(),color.getCode(),color.getId()) != 0;
+        return jdbc.update(UPDATE_COLORS_BY_ID, color.getName(), color.getCode(), color.getId()) != 0;
     }
 
     private static final String INSERT_COLOR = "INSERT INTO color(name,color_code) VALUES(?,?)";
+
     @Override
     public boolean saveColor(Color color) {
-        return jdbc.update(INSERT_COLOR,color.getName(),color.getCode()) != 0;
+        return jdbc.update(INSERT_COLOR, color.getName(), color.getCode()) != 0;
     }
 }
