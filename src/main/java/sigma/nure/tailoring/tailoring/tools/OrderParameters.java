42e5d566b77e78be3c08933738a647ca8193968c
Vladyslav Mormul <vladyslav.mormul@nure.ua>
2022-09-07 20:42:45 +0300

    added class for order parameters #9

---
+++ src/main/java/sigma/nure/tailoring/tailoring/tools/OrderParameters.java
@@ -0,0 +1,35 @@
+package sigma.nure.tailoring.tailoring.tools;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import sigma.nure.tailoring.tailoring.entities.OrderPaymentStatus;
+import sigma.nure.tailoring.tailoring.entities.OrderStatus;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class OrderParameters {
+    private Iterable<Long> ids;
+    private Iterable<Long> materialIds;
+    private Iterable<Long> colorIds;
+    private Iterable<Long> userIds;
+    private Iterable<OrderStatus> orderStatuses;
+    private Iterable<OrderPaymentStatus> paymentStatuses;
+    private String addressForSendContaining;
+    private String userPhoneNumberContaining;
+    private String userCityContaining;
+    private String userCountryContaining;
+    private String userFirstnameContaining;
+    private LocalDate beforeEndDate,afterEndDate;
+    private LocalDateTime beforeDateOfCreation,afterDareOfCreation;
+    private Boolean userIsMale;
+    private Boolean isFromTemplate;
+    private Integer startCost,endCost;
+    private Integer startCount,endCount;
+}
