4e7d9277fd86d13a2656684ce8f50cf462bc317a
Vladyslav Mormul <vladyslav.mormul@nure.ua>
2022-09-07 20:03:39 +0300

    added page for repository

---
+++ src/main/java/sigma/nure/tailoring/tailoring/tools/Page.java
@@ -0,0 +1,37 @@
+package sigma.nure.tailoring.tailoring.tools;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Page {
+    private String orderBy;
+    private Long limit;
+    private Long offset;
+    private Direction direction;
+
+    public String getOrderByOrDefault(String defaultValue) {
+        return orderBy != null ? orderBy : defaultValue;
+    }
+
+    public Long getLimitOrDefault(Long defaultValue) {
+        return limit != null ? limit : defaultValue;
+    }
+
+    public Long getOffsetOrDefault(Long defaultValue) {
+        return offset != null ? offset : defaultValue;
+    }
+
+    public Direction getDirectionOrDefault(Direction defaultValue) {
+        return direction != null ? direction : defaultValue;
+    }
+
+    public enum Direction {
+        DESC, ASC
+    }
+}
