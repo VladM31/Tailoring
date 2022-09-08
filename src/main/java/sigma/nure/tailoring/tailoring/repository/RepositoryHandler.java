6bcc4f1f8152668509fb4d3d5685b32b033ef65c
Vladyslav Mormul <vladyslav.mormul@nure.ua>
2022-09-08 14:10:00 +0300

    added dependency test container

--- src/main/java/sigma/nure/tailoring/tailoring/repository/RepositoryHandler.java
+++ src/main/java/sigma/nure/tailoring/tailoring/repository/RepositoryHandler.java
@@ -1,9 +1,13 @@
 package sigma.nure.tailoring.tailoring.repository;
 
+import org.springframework.lang.Nullable;
 import org.springframework.stereotype.Component;
+import sigma.nure.tailoring.tailoring.tools.Page;
 
 @Component
 public class RepositoryHandler {
+
+    public static final String ORDER_BY_AND_LIMIT = " ORDER BY %s %s LIMIT %s OFFSET %s";
 
     public <T> Iterable<T> getNullIfCollectionNullOrEmpty(Iterable<T> collection) {
         if (collection == null) {
@@ -13,5 +17,14 @@ public class RepositoryHandler {
             return null;
         }
         return collection;
+    }
+
+    public String getScriptFromPage(Page page, @Nullable String defaultOrderBy, @Nullable Page.Direction defaultDirection,
+                                    @Nullable Long defaultLimit, @Nullable Long defaultOffset) {
+        return String.format(ORDER_BY_AND_LIMIT,
+                page.getOrderByOrDefault(defaultOrderBy),
+                page.getDirectionOrDefault(defaultDirection),
+                page.getLimitOrDefault(defaultLimit),
+                page.getOffsetOrDefault(defaultOffset));
     }
 }
