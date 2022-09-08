4c7a1fb3a7cfddd19f8ca5ba8cc3707e9d09ec89
Vladyslav Mormul <vladyslav.mormul@nure.ua>
2022-09-08 23:58:02 +0300

    fixed naming parameters and added fields which are responsible for name of request parameters 2 #13

--- src/main/java/sigma/nure/tailoring/tailoring/service/HttpTelegramBotClient.java
+++ src/main/java/sigma/nure/tailoring/tailoring/service/HttpTelegramBotClient.java
@@ -3,8 +3,6 @@ package sigma.nure.tailoring.tailoring.service;
 import com.google.gson.Gson;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Value;
-import org.telegram.telegrambots.meta.api.objects.MessageEntity;
 import sigma.nure.tailoring.tailoring.entities.MessageForUser;
 
 import java.net.URI;
@@ -13,43 +11,49 @@ import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.nio.charset.StandardCharsets;
-import java.util.HashMap;
 import java.util.Map;
 
-public class TelegramBotServiceImplementedOnHttp implements TelegramBotService {
+public class HttpTelegramBotClient implements TelegramBotClient {
+    private static final Logger LOGGER = LoggerFactory.getLogger(HttpTelegramBotClient.class);
 
-    private static final Logger LOGGER = LoggerFactory.getLogger(TelegramBotServiceImplementedOnHttp.class);
+    private final Gson jsonConverter;
     private final String token;
+    private final String tokenParamName;
+    private final String phoneNumberParamName;
+    private final String jsonParamName;
     private final String urlHasNumber;
     private final String urlSendMessage;
-    private final Gson jsonConverter;
 
-    public TelegramBotServiceImplementedOnHttp(String token,
-                                               String urlHasNumber,
-                                               String urlSendMessage) {
+    public HttpTelegramBotClient(String token, String tokenParamName, String phoneNumberParamName,
+                                 String jsonParamName, String urlHasNumber, String urlSendMessage) {
         this.token = token;
+        this.tokenParamName = tokenParamName;
+        this.phoneNumberParamName = phoneNumberParamName;
+        this.jsonParamName = jsonParamName;
         this.urlSendMessage = urlSendMessage;
-        this.urlHasNumber = urlHasNumber + "?token=" + token + "&phoneNumber=";
+        this.urlHasNumber = urlHasNumber;
         this.jsonConverter = new Gson();
     }
-
 
     @Override
     public boolean hasPhoneNumber(String phoneNumber) {
         HttpRequest httpRequest = HttpRequest.newBuilder()
-                .uri(URI.create(urlHasNumber + phoneNumber))
+                .uri(URI.create(this.urlHasNumber +
+                        "?" + this.tokenParamName + "=" + this.token +
+                        "&" + this.phoneNumberParamName + "=" + phoneNumber))
                 .GET()
                 .build();
 
-        return sendRequestAndReturnBooleanAnswer(httpRequest,
-                "Telegram Bot Service: response phone number has error");
+        return send(httpRequest,
+                "Telegram Bot Client: response phone number has error");
     }
 
     @Override
     public boolean sendMessage(MessageForUser message) {
-        final Map<String, String> values = new HashMap<>();
-        values.put("token", this.token);
-        values.put("json", this.jsonConverter.toJson(message));
+        final Map<String, String> values = Map.of(
+                this.tokenParamName, this.token,
+                this.jsonParamName, this.jsonConverter.toJson(message)
+        );
 
         HttpRequest httpRequest = HttpRequest.newBuilder()
                 .uri(URI.create(this.urlSendMessage))
@@ -57,11 +61,11 @@ public class TelegramBotServiceImplementedOnHttp implements TelegramBotService {
                 .header("Content-Type", "application/x-www-form-urlencoded")
                 .build();
 
-        return sendRequestAndReturnBooleanAnswer(httpRequest,
-                "Telegram Bot Service: response send message has error");
+        return send(httpRequest,
+                "Telegram Bot Client: response send message has error");
     }
 
-    private static boolean sendRequestAndReturnBooleanAnswer(HttpRequest httpRequest, String messageError) {
+    private static boolean send(HttpRequest httpRequest, String messageError) {
         HttpClient client = HttpClient.newBuilder().build();
         HttpResponse<String> response = null;
 
@@ -87,5 +91,4 @@ public class TelegramBotServiceImplementedOnHttp implements TelegramBotService {
         }
         return HttpRequest.BodyPublishers.ofString(builder.toString());
     }
-
 }
--- src/test/java/sigma/nure/tailoring/tailoring/TailoringApplicationTests.java
+++ src/test/java/sigma/nure/tailoring/tailoring/TailoringApplicationTests.java
@@ -1,24 +1,13 @@
 package sigma.nure.tailoring.tailoring;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import sigma.nure.tailoring.tailoring.entities.OrderPaymentStatus;
-import sigma.nure.tailoring.tailoring.entities.OrderStatus;
-//import sigma.nure.tailoring.tailoring.repository.OrderRepository;
-//import sigma.nure.tailoring.tailoring.tools.OrderParameters;
-//import sigma.nure.tailoring.tailoring.tools.Page;
-
-import java.util.List;
 
 @SpringBootTest
 class TailoringApplicationTests {
-//    @Autowired
-//    private OrderRepository repository;
+
     @Test
     void contextLoads() {
-
-
     }
 
 }
