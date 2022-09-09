c585296373d6f6a62ae159e723b4b3f720f69e34
Vladyslav Mormul <vladyslav.mormul@nure.ua>
2022-09-09 21:17:58 +0300

    added service interface #50

--- src/main/java/sigma/nure/tailoring/tailoring/config/RepositoryConfig.java
+++ src/main/java/sigma/nure/tailoring/tailoring/config/RepositoryConfig.java
@@ -3,9 +3,7 @@ package sigma.nure.tailoring.tailoring.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.core.JdbcTemplate;
-import sigma.nure.tailoring.tailoring.repository.RepositoryHandler;
-import sigma.nure.tailoring.tailoring.repository.UserRepository;
-import sigma.nure.tailoring.tailoring.repository.UserRepositoryJdbcTemplatePostgres;
+import sigma.nure.tailoring.tailoring.repository.*;
 
 @Configuration
 public class RepositoryConfig {
@@ -13,5 +11,10 @@ public class RepositoryConfig {
     @Bean
     public UserRepository jdbcTemplateUserRepository(JdbcTemplate jdbc, RepositoryHandler handler) {
         return new UserRepositoryJdbcTemplatePostgres(jdbc, handler);
+    }
+
+    @Bean
+    public MaterialsRepository JdbcTemplateMaterialsRepository(JdbcTemplate jdbc){
+        return new MaterialsRepositoryJdbcTemplatePostgres(jdbc);
     }
 }
