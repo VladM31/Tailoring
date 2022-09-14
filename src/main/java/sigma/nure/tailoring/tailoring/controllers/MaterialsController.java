08e4e85643e68ae72e2a6a83fe8e9cb568ff6876
Vladyslav Mormul <vladyslav.mormul@nure.ua>
2022-09-14 20:35:21 +0300

    updated controller and fixed #30

--- src/main/java/sigma/nure/tailoring/tailoring/controllers/MaterialsController.java
+++ src/main/java/sigma/nure/tailoring/tailoring/controllers/MaterialsController.java
@@ -1,5 +1,7 @@
 package sigma.nure.tailoring.tailoring.controllers;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
@@ -7,68 +9,91 @@ import org.springframework.web.bind.annotation.*;
 import sigma.nure.tailoring.tailoring.entities.Color;
 import sigma.nure.tailoring.tailoring.entities.Material;
 import sigma.nure.tailoring.tailoring.service.MaterialsService;
+import sigma.nure.tailoring.tailoring.tools.Answer;
 import sigma.nure.tailoring.tailoring.tools.ColorForm;
 import sigma.nure.tailoring.tailoring.tools.MaterialForm;
 
 import javax.validation.Valid;
 import javax.validation.constraints.NotNull;
-import java.util.ArrayList;
 import java.util.List;
 
 @RestController
-@RequestMapping("/rest/api/v1")
+@PropertySource("classpath:properties/rest-resources.properties")
+@RequestMapping("${material.url.resources}")
 public class MaterialsController {
 
+    private static final int FIRST_ERROR = 0;
+    private static final String EMPTY_DEFAULT_MESSAGE = "";
+    private final String errorMessage;
     private final MaterialsService materialsService;
 
-    public MaterialsController(MaterialsService materialsService) {
+    public MaterialsController(MaterialsService materialsService, @Value("${error.message}") String errorMessage) {
+        this.errorMessage = errorMessage;
         this.materialsService = materialsService;
     }
 
-    @GetMapping(value = {"/colors", "/free/colors"})
+    @GetMapping(value = {"/colors"})
     public ResponseEntity<List<Color>> getAllColors() {
-        return new ResponseEntity<>(materialsService.findAllColors(), HttpStatus.ACCEPTED);
+        return new ResponseEntity<>(materialsService.findAllColors(), HttpStatus.OK);
     }
 
     @PostMapping(value = "/colors")
-    public ResponseEntity<Boolean> saveColor(@Valid ColorForm form, @NotNull BindingResult bindingResult) {
+    public ResponseEntity<Answer<Boolean>> saveColor(@Valid ColorForm form, @NotNull BindingResult bindingResult) {
         if (bindingResult.hasErrors()) {
-            return new ResponseEntity<>(false, HttpStatus.NOT_ACCEPTABLE);
+            return createErrorResponseWithStatus400(bindingResult, EMPTY_DEFAULT_MESSAGE);
         }
         final boolean wasSave = this.materialsService.saveColor(form.toColor());
-        return new ResponseEntity<>(wasSave, wasSave ? HttpStatus.CREATED : HttpStatus.NOT_ACCEPTABLE);
+        return this.getResponse(wasSave);
     }
 
     @PutMapping(value = "/colors")
-    public ResponseEntity<Boolean> updateColor(@Valid ColorForm form, @NotNull BindingResult bindingResult) {
+    public ResponseEntity<Answer<Boolean>> updateColor(@Valid ColorForm form, @NotNull BindingResult bindingResult) {
         if (bindingResult.hasErrors() || form.getId() == null) {
-            return new ResponseEntity<>(false, HttpStatus.NOT_MODIFIED);
+            return createErrorResponseWithStatus400(bindingResult, "Id is null");
         }
         final boolean wasUpdate = this.materialsService.updateColor(form.toColor());
-        return new ResponseEntity<>(wasUpdate, wasUpdate ? HttpStatus.ACCEPTED : HttpStatus.NOT_MODIFIED);
+        return this.getResponse(wasUpdate);
     }
 
 
-    @GetMapping(value = {"/materials", "/free/materials"})
+    @GetMapping(value = {"/materials"})
     public ResponseEntity<List<Material>> getAllMaterials(){
-        return new ResponseEntity<>(this.materialsService.findAllMaterial(), HttpStatus.ACCEPTED);
+        return new ResponseEntity<>(this.materialsService.findAllMaterial(), HttpStatus.OK);
     }
 
     @PostMapping(value = "/materials")
-    public ResponseEntity<Boolean> saveMaterial(@Valid MaterialForm form, @NotNull BindingResult bindingResult) {
+    public ResponseEntity<Answer<Boolean>> saveMaterial(@Valid MaterialForm form, @NotNull BindingResult bindingResult) {
         if (bindingResult.hasErrors()) {
-            return new ResponseEntity<>(false, HttpStatus.NOT_ACCEPTABLE);
+            return createErrorResponseWithStatus400(bindingResult, EMPTY_DEFAULT_MESSAGE);
         }
         final boolean wasSave = this.materialsService.saveMaterial(form.toMaterial());
-        return new ResponseEntity<>(wasSave, wasSave ? HttpStatus.CREATED : HttpStatus.NOT_ACCEPTABLE);
+        return this.getResponse(wasSave);
     }
 
     @PutMapping(value = "/materials")
-    public ResponseEntity<Boolean> updateMaterial(@Valid MaterialForm form, @NotNull BindingResult bindingResult) {
+    public ResponseEntity<Answer<Boolean>> updateMaterial(@Valid MaterialForm form, @NotNull BindingResult bindingResult) {
         if (bindingResult.hasErrors() || form.getId() == null) {
-            return new ResponseEntity<>(false, HttpStatus.NOT_MODIFIED);
+            return createErrorResponseWithStatus400(bindingResult, "Id is null");
         }
         final boolean wasUpdate = this.materialsService.updateMaterial(form.toMaterial());
-        return new ResponseEntity<>(wasUpdate, wasUpdate ? HttpStatus.ACCEPTED : HttpStatus.NOT_MODIFIED);
+        return this.getResponse(wasUpdate);
+    }
+
+    private ResponseEntity<Answer<Boolean>> createErrorResponseWithStatus400(@NotNull BindingResult bindingResult, String defaultMessage) {
+        if (bindingResult.hasErrors()) {
+            return new ResponseEntity<>(new Answer(false, bindingResult
+                    .getFieldErrors()
+                    .get(FIRST_ERROR)
+                    .getDefaultMessage()
+            ), HttpStatus.BAD_REQUEST);
+        }
+        return new ResponseEntity<>(new Answer<>(false, defaultMessage), HttpStatus.BAD_REQUEST);
+    }
+
+    private ResponseEntity<Answer<Boolean>> getResponse(boolean hasNotError) {
+        return new ResponseEntity<>(
+                new Answer<>(hasNotError, hasNotError ? "" : errorMessage),
+                hasNotError ? HttpStatus.OK : HttpStatus.INTERNAL_SERVER_ERROR
+        );
     }
 }
