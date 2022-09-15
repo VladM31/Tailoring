9c5f95d91a4e826d2252a09909ab2920ae834601
Vladyslav Mormul <vladyslav.mormul@nure.ua>
2022-09-15 13:31:56 +0300

    created ServiceConfig with time annotation  #51

---
+++ src/main/java/sigma/nure/tailoring/tailoring/config/ServiceConfig.java
@@ -0,0 +1,19 @@
+package sigma.nure.tailoring.tailoring.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import sigma.nure.tailoring.tailoring.repository.OrderRepository;
+import sigma.nure.tailoring.tailoring.repository.TailoringTemplateRepository;
+import sigma.nure.tailoring.tailoring.service.PopularTemplateService;
+import sigma.nure.tailoring.tailoring.service.PopularTemplateServiceImpl;
+
+@Configuration
+@EnableScheduling
+public class ServiceConfig {
+
+    @Bean
+    public PopularTemplateService popularTemplateServiceImpl(OrderRepository orderRepository, TailoringTemplateRepository templateRepository) {
+        return new PopularTemplateServiceImpl(orderRepository, templateRepository);
+    }
+}
