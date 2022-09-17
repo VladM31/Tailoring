ec84141e598ec84b83c4211066b15fd993caabef
Vladyslav Mormul <vladyslav.mormul@nure.ua>
2022-09-17 18:45:48 +0300

    added logic for catch DuplicateKeyException #30

--- src/main/java/sigma/nure/tailoring/tailoring/controllers/RestResponseEntityExceptionHandler.java
+++ src/main/java/sigma/nure/tailoring/tailoring/controllers/RestResponseEntityExceptionHandler.java
@@ -2,36 +2,27 @@ package sigma.nure.tailoring.tailoring.controllers;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.HttpHeaders;
+import org.springframework.dao.DuplicateKeyException;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
-import sigma.nure.tailoring.tailoring.tools.ResponseException;
 
 @ControllerAdvice
 public class RestResponseEntityExceptionHandler extends ResponseEntityExceptionHandler {
     private static final Logger LOGGER = LoggerFactory.getLogger(RestResponseEntityExceptionHandler.class);
+    private static final String ERROR_DUPLICATE_KEY_MESSAGE_RESPONSE = "Sorry, you send duplicate data";
+    private static final String WAR_DUPLICATE_KEY_MESSAGE_LOG = "Was send duplicate data";
 
-    private final String errorMessage;
+    @ExceptionHandler(DuplicateKeyException.class)
+    @ResponseBody
+    public ResponseEntity handleEDuplicateKeyException(DuplicateKeyException ex, WebRequest request) {
+        LOGGER.warn(WAR_DUPLICATE_KEY_MESSAGE_LOG, ex);
 
-    public RestResponseEntityExceptionHandler(@Value("${error.message}") String errorMessage) {
-        this.errorMessage = errorMessage;
+        return new ResponseEntity<>(ERROR_DUPLICATE_KEY_MESSAGE_RESPONSE, HttpStatus.BAD_REQUEST);
     }
 
-
-    @ExceptionHandler(value
-            = {ResponseException.class})
-    protected ResponseEntity<Object> handleConflict(RuntimeException ex, WebRequest request) {
-        ResponseException exception = (ResponseException) ex;
-
-        exception.log(LOGGER);
-
-        String message = exception.getAnswerMessage() == null ? errorMessage : exception.getAnswerMessage();
-
-        return handleExceptionInternal(exception, message,
-                new HttpHeaders(), exception.getStatus(), request);
-    }
 }
