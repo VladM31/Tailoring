5ddffef5336c49ca9872c1e46ee3c1200043ae6e
Vladyslav Mormul <vladyslav.mormul@nure.ua>
2022-09-17 12:34:57 +0300

    updated valid annotation #30

--- src/main/java/sigma/nure/tailoring/tailoring/tools/ColorForm.java
+++ src/main/java/sigma/nure/tailoring/tailoring/tools/ColorForm.java
@@ -7,6 +7,7 @@ import org.springframework.lang.Nullable;
 import sigma.nure.tailoring.tailoring.entities.Color;
 
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
 
@@ -14,14 +15,16 @@ import javax.validation.constraints.Size;
 @NoArgsConstructor
 @AllArgsConstructor
 public class ColorForm {
-    @Nullable
+
     private Integer id;
     @Size(min = 2, max = 20, message = "Name must be greater than 2 and less than 20 characters")
     @NotBlank(message = "Name must not be empty")
+    @NotNull(message = "Name mustn't be null")
     private String name;
-    @Pattern(regexp = "[0-9A-Fa-b].")
+    @Pattern(regexp = "[0-9A-Fa-f]{6}")
     @Size(min = 6, max = 6, message = "Code does not consist of 6 characters")
     @NotBlank(message = "Code must not be empty")
+    @NotNull(message = "Code mustn't be null")
     private String code;
 
     public Color toColor() {
--- src/main/java/sigma/nure/tailoring/tailoring/tools/MaterialForm.java
+++ src/main/java/sigma/nure/tailoring/tailoring/tools/MaterialForm.java
@@ -8,6 +8,7 @@ import sigma.nure.tailoring.tailoring.entities.Material;
 
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
 @Data
@@ -18,8 +19,10 @@ public class MaterialForm {
     private Integer id;
     @Size(min = 2, max = 60, message = "Name must be greater than 2 and less than 60 characters")
     @NotBlank(message = "Name must not be empty")
+    @NotNull(message = "Name mustn't be null")
     private String name;
     @Min(message = "Cost can not be less than 1", value = 1)
+    @NotNull(message = "Cost mustn't be null")
     private Integer cost;
 
     public Material toMaterial() {
