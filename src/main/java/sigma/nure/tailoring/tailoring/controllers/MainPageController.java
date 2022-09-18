05c674b073fb583172e976b6bc302e566e44694c
Vladyslav Mormul <vladyslav.mormul@nure.ua>
2022-09-19 00:40:03 +0300

    created MainPageController #17

---
+++ src/main/java/sigma/nure/tailoring/tailoring/controllers/MainPageController.java
@@ -0,0 +1,38 @@
+package sigma.nure.tailoring.tailoring.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import sigma.nure.tailoring.tailoring.service.PopularTemplateService;
+
+import java.util.function.Function;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+@Controller
+public class MainPageController {
+    private final PopularTemplateService popularTemplateService;
+    private final HandlerForControllers handler;
+    private static final Function<Stream<String>,String> findFrontImage =
+            (images) -> images.filter(img -> img.contains("front-")).findFirst().orElse("Error.png");
+
+    public MainPageController(PopularTemplateService popularTemplateService, HandlerForControllers handler) {
+        this.popularTemplateService = popularTemplateService;
+        this.handler = handler;
+    }
+
+    @GetMapping("/")
+    public String showMainPage(Model model){
+
+
+        model.addAttribute("findFrontImg",findFrontImage);
+        model.addAttribute("popularTemplates",popularTemplateService
+                .getPopularTailoringTemplate()
+                .stream()
+                .limit(3)
+                .collect(Collectors.toList()));
+
+        return "MainPage";
+    }
+}
