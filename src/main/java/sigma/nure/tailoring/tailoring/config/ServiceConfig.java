0b3ca88bf5dd95465299422e6da7b5a512316839
Vladyslav Mormul <vladyslav.mormul@nure.ua>
2022-09-19 15:52:23 +0300

    fixed names in service and repository #66

--- src/main/java/sigma/nure/tailoring/tailoring/config/ServiceConfig.java
+++ src/main/java/sigma/nure/tailoring/tailoring/config/ServiceConfig.java
@@ -1,12 +1,16 @@
 package sigma.nure.tailoring.tailoring.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import sigma.nure.tailoring.tailoring.repository.OrderRepository;
 import sigma.nure.tailoring.tailoring.repository.TailoringTemplateRepository;
+import sigma.nure.tailoring.tailoring.repository.UserRepository;
 import sigma.nure.tailoring.tailoring.service.PopularTemplateService;
 import sigma.nure.tailoring.tailoring.service.PopularTemplateServiceImpl;
+import sigma.nure.tailoring.tailoring.service.UserService;
+import sigma.nure.tailoring.tailoring.service.UserServiceImpl;
 
 @Configuration
 @EnableScheduling
@@ -15,5 +19,11 @@ public class ServiceConfig {
     @Bean
     public PopularTemplateService popularTemplateServiceImpl(OrderRepository orderRepository, TailoringTemplateRepository templateRepository) {
         return new PopularTemplateServiceImpl(orderRepository, templateRepository);
+    }
+
+    @Bean
+    public UserService userServiceImpl(UserRepository userRepository,
+                                       @Value("${minutes.waiting.for.user.registration}") long minutesForWork){
+        return new UserServiceImpl(userRepository,minutesForWork);
     }
 }
--- src/main/java/sigma/nure/tailoring/tailoring/service/UserServiceImpl.java
+++ src/main/java/sigma/nure/tailoring/tailoring/service/UserServiceImpl.java
@@ -4,12 +4,16 @@ import sigma.nure.tailoring.tailoring.entities.User;
 import sigma.nure.tailoring.tailoring.repository.UserRepository;
 import sigma.nure.tailoring.tailoring.tools.Page;
 import sigma.nure.tailoring.tailoring.tools.UserSearchCriteria;
+import sigma.nure.tailoring.tailoring.tools.UserSortColumn;
 
 import java.time.LocalDateTime;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 
 public class UserServiceImpl implements UserService {
+    private static final Map<String,String> SORT_COLUMN_CONVECTOR = new HashMap<>();
     private final UserRepository repository;
     private final long minutesForWork;
 
@@ -20,6 +24,7 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public List<User> findBy(UserSearchCriteria userParameters, Page pageable) {
+        pageable.setOrderBy(SORT_COLUMN_CONVECTOR.getOrDefault(pageable.getOrderBy(),"u.date_registration"));
         return repository.findBy(userParameters, pageable);
     }
 
@@ -41,5 +46,19 @@ public class UserServiceImpl implements UserService {
     @Override
     public boolean update(User user) {
         return repository.update(user);
+    }
+
+    {
+        SORT_COLUMN_CONVECTOR.put("city","u.city");
+        SORT_COLUMN_CONVECTOR.put("male","u.male");
+        SORT_COLUMN_CONVECTOR.put("email","u.email");
+        SORT_COLUMN_CONVECTOR.put("active","u.active");
+        SORT_COLUMN_CONVECTOR.put("country","u.country");
+        SORT_COLUMN_CONVECTOR.put("lastname","u.lastname");
+        SORT_COLUMN_CONVECTOR.put("firstname","u.firstname");
+        SORT_COLUMN_CONVECTOR.put("roleName","r.name");
+        SORT_COLUMN_CONVECTOR.put("userState","u.user_state");
+        SORT_COLUMN_CONVECTOR.put("phoneNumber","u.phone_number");
+        SORT_COLUMN_CONVECTOR.put("dateRegistration","u.date_registration");
     }
 }
