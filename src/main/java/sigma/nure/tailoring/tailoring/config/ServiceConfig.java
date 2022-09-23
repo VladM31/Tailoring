2602cc301899ee389cb9e77074b0f1e2aada7496
Vladyslav Mormul <vladyslav.mormul@nure.ua>
2022-09-23 20:39:40 +0300

    added service to config #48

--- src/main/java/sigma/nure/tailoring/tailoring/config/ServiceConfig.java
+++ src/main/java/sigma/nure/tailoring/tailoring/config/ServiceConfig.java
@@ -1,12 +1,19 @@
 package sigma.nure.tailoring.tailoring.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import sigma.nure.tailoring.tailoring.converters.FileConverter;
+import sigma.nure.tailoring.tailoring.converters.TailoringTemplateConvertor;
+import sigma.nure.tailoring.tailoring.converters.TailoringTemplateSortColumnConverter;
+import sigma.nure.tailoring.tailoring.repository.MaterialsRepository;
 import sigma.nure.tailoring.tailoring.repository.OrderRepository;
 import sigma.nure.tailoring.tailoring.repository.TailoringTemplateRepository;
 import sigma.nure.tailoring.tailoring.service.PopularTemplateService;
 import sigma.nure.tailoring.tailoring.service.PopularTemplateServiceImpl;
+import sigma.nure.tailoring.tailoring.service.TailoringTemplateService;
+import sigma.nure.tailoring.tailoring.service.TailoringTemplateServiceImpl;
 
 @Configuration
 @EnableScheduling
@@ -15,5 +22,18 @@ public class ServiceConfig {
     @Bean
     public PopularTemplateService popularTemplateServiceImpl(OrderRepository orderRepository, TailoringTemplateRepository templateRepository) {
         return new PopularTemplateServiceImpl(orderRepository, templateRepository);
+    }
+
+    @Bean
+    public TailoringTemplateService tailoringTemplateServiceImpl(
+            FileConverter fileConverter,
+            TailoringTemplateRepository templateRepository,
+            @Value("${template.image.directory}") String imagesDirectory,
+            TailoringTemplateSortColumnConverter tailoringTemplateSortColumnConverter,
+            TailoringTemplateConvertor tailoringTemplateConvertor,
+            MaterialsRepository materialsRepository) {
+
+        return new TailoringTemplateServiceImpl(fileConverter, templateRepository, imagesDirectory,
+                materialsRepository, tailoringTemplateSortColumnConverter, tailoringTemplateConvertor);
     }
 }
