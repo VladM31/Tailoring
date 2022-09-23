7ad9d055d932e841c1be4b1cb514fd1c9fbaab11
Vladyslav Mormul <vladyslav.mormul@nure.ua>
2022-09-23 20:40:25 +0300

    added handle for FormException #48

--- src/main/java/sigma/nure/tailoring/tailoring/controllers/RestResponseEntityExceptionHandler.java
+++ src/main/java/sigma/nure/tailoring/tailoring/controllers/RestResponseEntityExceptionHandler.java
@@ -5,11 +5,14 @@ import org.slf4j.LoggerFactory;
 import org.springframework.dao.DuplicateKeyException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+import sigma.nure.tailoring.tailoring.exceptions.FormException;
+import sigma.nure.tailoring.tailoring.tools.TailoringTemplateForm;
 
 @ControllerAdvice
 public class RestResponseEntityExceptionHandler extends ResponseEntityExceptionHandler {
@@ -23,6 +26,13 @@ public class RestResponseEntityExceptionHandler extends ResponseEntityExceptionH
         LOGGER.warn(WAR_DUPLICATE_KEY_MESSAGE_LOG, ex);
 
         return new ResponseEntity<>(ERROR_DUPLICATE_KEY_MESSAGE_RESPONSE, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(FormException.class)
+    public String handleFormException(FormException exception, Model model) {
+        model.addAttribute("errors", exception.getMessage());
+        model.addAttribute(exception.getFormName(), exception.getForm());
+        return exception.getPageName();
     }
 
 }
