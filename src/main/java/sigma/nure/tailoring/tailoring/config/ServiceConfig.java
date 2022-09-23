dfc46b2c900f3dd66628eac8e104666339486066
Vladyslav Mormul <vladyslav.mormul@nure.ua>
2022-09-24 00:40:02 +0300

    added config for TelegramBotClient

--- src/main/java/sigma/nure/tailoring/tailoring/config/ServiceConfig.java
+++ src/main/java/sigma/nure/tailoring/tailoring/config/ServiceConfig.java
@@ -1,12 +1,21 @@
 package sigma.nure.tailoring.tailoring.config;
 
+import lombok.SneakyThrows;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.telegram.telegrambots.meta.api.objects.MessageEntity;
+import sigma.nure.tailoring.tailoring.entities.MessageForUser;
 import sigma.nure.tailoring.tailoring.repository.OrderRepository;
 import sigma.nure.tailoring.tailoring.repository.TailoringTemplateRepository;
+import sigma.nure.tailoring.tailoring.service.HttpTelegramBotClient;
 import sigma.nure.tailoring.tailoring.service.PopularTemplateService;
 import sigma.nure.tailoring.tailoring.service.PopularTemplateServiceImpl;
+import sigma.nure.tailoring.tailoring.service.TelegramBotClient;
+
+import java.util.List;
+import java.util.Random;
 
 @Configuration
 @EnableScheduling
@@ -15,5 +24,16 @@ public class ServiceConfig {
     @Bean
     public PopularTemplateService popularTemplateServiceImpl(OrderRepository orderRepository, TailoringTemplateRepository templateRepository) {
         return new PopularTemplateServiceImpl(orderRepository, templateRepository);
+    }
+
+    @Bean
+    public TelegramBotClient httpTelegramBotClient(@Value("${telegram.bot.connector.token}") String token,
+                                                   @Value("${token.param.name}") String tokenParamName,
+                                                   @Value("${phone.number.param.name}") String phoneNumberParamName,
+                                                   @Value("${json.param.name}") String jsonParamName,
+                                                   @Value("${telegram.bot.url.has.phone.number}") String telegramBotDbUrl,
+                                                   @Value("${telegram.bot.url.has.send.message}") String telegramBotUrl) {
+        return new HttpTelegramBotClient(token, tokenParamName, phoneNumberParamName,
+                jsonParamName, telegramBotDbUrl, telegramBotUrl);
     }
 }
