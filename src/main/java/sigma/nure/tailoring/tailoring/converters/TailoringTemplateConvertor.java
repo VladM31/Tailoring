3130b6a2a2d52fe0955dc8f3e1291345a507fd43
Vladyslav Mormul <vladyslav.mormul@nure.ua>
2022-09-23 20:38:27 +0300

    added template converter #48

---
+++ src/main/java/sigma/nure/tailoring/tailoring/converters/TailoringTemplateConvertor.java
@@ -0,0 +1,71 @@
+package sigma.nure.tailoring.tailoring.converters;
+
+import org.springframework.stereotype.Component;
+import sigma.nure.tailoring.tailoring.entities.Color;
+import sigma.nure.tailoring.tailoring.entities.Material;
+import sigma.nure.tailoring.tailoring.entities.TailoringTemplate;
+import sigma.nure.tailoring.tailoring.entities.TailoringTemplateWithMaterialIds;
+import sigma.nure.tailoring.tailoring.repository.MaterialsRepository;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+@Component
+public class TailoringTemplateConvertor {
+
+    public Function<TailoringTemplateWithMaterialIds, TailoringTemplate> getConverter(MaterialsRepository materialsRepository) {
+
+        final var toMaterial = createMaterialsConverter(materialsRepository.findAllMaterial());
+
+        final var toColors = createColorsConverter(materialsRepository.findAllColors());
+
+        return (templateFromRepo) -> {
+            TailoringTemplate template = new TailoringTemplate();
+
+            template.setId(templateFromRepo.getId());
+            template.setName(templateFromRepo.getName());
+            template.setActive(templateFromRepo.isActive());
+            template.setDateOfCreation(templateFromRepo.getDateOfCreation());
+            template.setCost(templateFromRepo.getCost());
+            template.setTypeTemplate(templateFromRepo.getTypeTemplate());
+            template.setTemplateDescription(templateFromRepo.getTemplateDescription());
+
+            template.setMaterials(toMaterial.apply(templateFromRepo.getMaterialIds()));
+            template.setColors(toColors.apply(templateFromRepo.getColorIds()));
+
+            template.setImagesUrl(templateFromRepo.getImagesUrl());
+            template.setPartSizeForTemplates(templateFromRepo.getPartSizeForTemplates());
+
+            return template;
+        };
+    }
+
+
+    private Function<Iterable<Integer>, Set<Material>> createMaterialsConverter(List<Material> materialList) {
+        final Map<Integer, Material> materialMap = materialList.stream().collect(Collectors.toMap(m -> m.getId(), m -> m));
+
+        return (ids) -> {
+            Set<Material> materials = new HashSet<>();
+
+            ids.forEach(id -> materials.add(materialMap.get(id)));
+
+            return materials;
+        };
+    }
+
+    private Function<Iterable<Integer>, Set<Color>> createColorsConverter(List<Color> colorList) {
+        final Map<Integer, Color> colorMap = colorList.stream().collect(Collectors.toMap(c -> c.getId(), c -> c));
+
+        return (ids) -> {
+            Set<Color> colors = new HashSet<>();
+
+            ids.forEach(id -> colors.add(colorMap.get(id)));
+
+            return colors;
+        };
+    }
+}
