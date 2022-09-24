08c60a718eaafe90de5b7dafaa8964688b9f3cc7
Vladyslav Mormul <vladyslav.mormul@nure.ua>
2022-09-24 16:12:11 +0300

    added service to config #33

--- src/main/java/sigma/nure/tailoring/tailoring/config/ServiceConfig.java
+++ src/main/java/sigma/nure/tailoring/tailoring/config/ServiceConfig.java
@@ -1,5 +1,6 @@
 package sigma.nure.tailoring.tailoring.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableScheduling;
@@ -8,14 +9,7 @@ import sigma.nure.tailoring.tailoring.repository.OrderRepository;
 import sigma.nure.tailoring.tailoring.repository.TailoringTemplateRepository;
 import sigma.nure.tailoring.tailoring.repository.UserCodeRepository;
 import sigma.nure.tailoring.tailoring.repository.UserRepository;
-import sigma.nure.tailoring.tailoring.service.PopularTemplateService;
-import sigma.nure.tailoring.tailoring.service.PopularTemplateServiceImpl;
-import sigma.nure.tailoring.tailoring.service.UserCodeService;
-import sigma.nure.tailoring.tailoring.service.UserCodeServiceImpl;
-import sigma.nure.tailoring.tailoring.service.UserService;
-import sigma.nure.tailoring.tailoring.service.UserServiceImpl;
-
-import sigma.nure.tailoring.tailoring.service.TelegramBotClient;
+import sigma.nure.tailoring.tailoring.service.*;
 
 import java.util.List;
 import java.util.Random;
@@ -43,11 +37,17 @@ public class ServiceConfig {
     @Bean
     public UserCodeService userCodeService(UserCodeRepository userCodeRepository) {
         return new UserCodeServiceImpl(userCodeRepository);
-        }
+    }
 
+    @Bean
     public UserService userServiceImpl(UserRepository userRepository,
                                        @Value("${minutes.waiting.for.user.registration}") long minutesForWork,
                                        UserServiceSortColumnConverter converter) {
         return new UserServiceImpl(converter, userRepository, minutesForWork);
+    }
+
+    @Bean
+    public SecurityService securityServiceImpl(UserService userService, UserCodeService userCodeService, TelegramBotClient telegramBotClient) {
+        return new SecurityServiceImpl(userService, userCodeService, telegramBotClient);
     }
 }
