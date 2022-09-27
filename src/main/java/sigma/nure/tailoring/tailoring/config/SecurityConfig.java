2e94ab44115f203d816bdf466112233a2e303e10
Vladyslav Mormul <vladyslav.mormul@nure.ua>
2022-09-27 22:58:49 +0300

    added true Security Config  #38

---
+++ src/main/java/sigma/nure/tailoring/tailoring/config/SecurityConfig.java
@@ -0,0 +1,97 @@
+package sigma.nure.tailoring.tailoring.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+    private static final int STRENGTH = 10;
+    private final UserDetailsService userService;
+
+    public SecurityConfig(UserDetailsService userService) {
+        this.userService = userService;
+    }
+
+    @Bean
+    public PasswordEncoder bcryptPasswordEncoder() {
+        return new BCryptPasswordEncoder(STRENGTH);
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http, PasswordEncoder passwordEncoder) throws Exception {
+
+        AuthenticationManagerBuilder authenticationManagerBuilder = http.getSharedObject(AuthenticationManagerBuilder.class);
+        authenticationManagerBuilder.userDetailsService(userService).passwordEncoder(passwordEncoder);
+        var authenticationManager = authenticationManagerBuilder.build();
+
+        http
+                .authorizeRequests()
+                .antMatchers("/**")
+                .permitAll()
+                .anyRequest()
+                .authenticated()
+                .and().csrf().disable()
+                .formLogin()
+                .loginPage("/authorization/login")
+                .failureUrl("/authorization/login?error=true")
+                .permitAll()
+                .defaultSuccessUrl("/", true)
+                .and()
+                .authenticationManager(authenticationManager)
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.ALWAYS)
+                .and()
+                .httpBasic();
+
+        return http.build();
+    }
+
+//    @Override
+//    protected void configure(HttpSecurity http) throws Exception {
+//        http
+//                .authorizeRequests()
+//                .antMatchers("/**")
+//                .permitAll()
+//                .anyRequest()
+//                .authenticated()
+//                .and().csrf().disable()
+//                .formLogin()
+//                .loginPage("/authorization/login")
+//                .failureUrl("/authorization/login?error=true")
+//                .permitAll()
+//                .defaultSuccessUrl("/",true);
+//
+//    }
+
+//    @Override
+//    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+//        auth.userDetailsService(userService).passwordEncoder(NoOpPasswordEncoder.getInstance());
+//    }
+//
+//
+//
+//    @Override
+//    public void configure(WebSecurity web) throws Exception {
+//        web
+//           .ignoring()
+//           .antMatchers("/resources/**", "/static/**", "/css/**", "/js/**", "/images/**");
+//    }
+
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        return (web) -> web
+                .ignoring()
+                .antMatchers("/resources/**", "/static/**", "/css/**", "/js/**", "/images/**");
+    }
+}
